8c9f7576c064d21dd05fc150551e3ba88be8078b
devjesse <devjessem@gmail.com>
2025-03-14 16:54:01 -0500

    updated 404 and added yarn

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,32 @@
+import fs from "fs";
+import path from "path";
 import Layout from "@components/layout/layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import ErrorInformation from "@components/404/error-information";
 import { GetStaticProps } from "next";
 
-export default function ErrorPage() {
-    return <ErrorInformation />;
+interface ErrorPageProps {
+    siteSettings: any; // Footer / Site Settings
 }
 
-ErrorPage.Layout = Layout;
+export default function ErrorPage({ siteSettings }: ErrorPageProps) {
+    return (
+        <Layout siteSettings={siteSettings}>
+            <ErrorInformation />
+        </Layout>
+    );
+}
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    // Read site settings from local JSON
+    const filePath = path.join(process.cwd(), "data", "wordpressData.json");
+    const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+    const siteSettings = jsonData?.nextJsSiteSettings || null;
+
     return {
         props: {
+            siteSettings, // Pass to Layout/Footer
             ...(await serverSideTranslations(locale!, [
                 "common",
                 "forms",
