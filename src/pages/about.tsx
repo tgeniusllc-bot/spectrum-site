5195f3a83e7b82a83b59b8166074df33554b135e
Kerem <tgeniusllc@gmail.com>
2026-05-07 16:31:33 +0300

    Remove long therapist sections from about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,38 +7,24 @@ import { GetStaticProps } from "next";
 import Loader from "@components/ui/loader/loader";
 import AboutHero from "@containers/about-hero";
 import AboutStats from "@containers/about-stats";
-import AboutImageSection from "@containers/about-image-section";
 import AboutValues from "@containers/about-values";
 import AboutOurTeam from "@containers/about-our-team";
-import AboutJoinOurTeam from "@containers/about-join-our-team";
 
 interface AboutPageProps {
-    data: any; // Or more specific type if you like
-    siteSettings: any; // Footer / Site Settings
+    data: any;
+    siteSettings: any;
 }
+
 export default function Home({ data, siteSettings }: AboutPageProps) {
     if (!data) {
         return <Loader />;
     }
 
-    const teamMembers =
-        data.acfAboutPage.aboutPageBlocks.aboutJoinOurTeamMembers || [];
-
     return (
         <Layout siteSettings={siteSettings}>
             <AboutHero query={{ nodeByUri: data }} />
             <AboutStats query={{ nodeByUri: data }} />
-            {/* <AboutImageSection query={{ nodeByUri: data }} /> */}
-            {/* Render first team member */}
-            {teamMembers.length > 0 && (
-                <AboutJoinOurTeam key={0} teamMemberData={teamMembers[0]} />
-            )}
-            {/* Render AboutValues after first team member */}
             <AboutValues query={{ nodeByUri: data }} />
-            {/* Render remaining team members */}
-            {teamMembers.slice(1).map((teamMember: any, index: number) => (
-                <AboutJoinOurTeam key={index + 1} teamMemberData={teamMember} />
-            ))}
             <AboutOurTeam query={{ nodeByUri: data }} />
             <Divider className="mb-0" />
         </Layout>
@@ -46,20 +32,16 @@ export default function Home({ data, siteSettings }: AboutPageProps) {
 }
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-    // 1) Read the data from the local JSON
     const filePath = path.join(process.cwd(), "data", "wordpressData.json");
     const jsonData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-    // 2) Extract the "home" portion from your JSON
-    // This depends on how your JSON is structured
-    const aboutData = jsonData.about; // or something like jsonData?.home
+    const aboutData = jsonData.about;
     const siteSettings = jsonData?.nextJsSiteSettings || null;
 
-    // 3) Return it as props
     return {
         props: {
             data: aboutData,
-            siteSettings, // Pass to Layout/Footer
+            siteSettings,
             ...(await serverSideTranslations(locale!, [
                 "common",
                 "forms",
