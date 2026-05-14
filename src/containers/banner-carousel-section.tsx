2397d46a3bdded2a652d88ca73b1f3bcb09d7844
Kerem <tgeniusllc@gmail.com>
2026-05-14 23:36:38 +0300

    Remove Get Started CTA

--- src/containers/banner-carousel-section.tsx
+++ src/containers/banner-carousel-section.tsx
@@ -50,7 +50,6 @@ const BannerCarouselSection: React.FC<Props> = ({ query }) => {
                            href="#"
                            className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                        >
-                           Get started
                        </a>
                        <a
                            href="#"
