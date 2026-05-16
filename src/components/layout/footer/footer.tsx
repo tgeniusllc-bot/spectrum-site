2520daa511a47cd0afbc9288bd2348e1df66cc75
Kerem <tgeniusllc@gmail.com>
2026-05-16 18:41:25 +0300

    test footer live update

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -6,6 +6,18 @@ const Footer: React.FC = () => (
     <footer className=" ">
         {/* <Widgets widgets={widgets} /> */}
         <Copyright />
+
+        <div
+            style={{
+                textAlign: "center",
+                fontSize: "12px",
+                paddingBottom: "10px",
+                color: "red",
+                fontWeight: "bold",
+            }}
+        >
+            FOOTER TEST KEREMIYO
+        </div>
     </footer>
 );
 
