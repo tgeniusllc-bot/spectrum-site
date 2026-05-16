9c27a55141c776d7a9eee182f8fc54b6cc6a34d8
Kerem <tgeniusllc@gmail.com>
2026-05-16 18:47:07 +0300

    Updated active footer Keremiyo link

--- src/components/layout/footer/footer-three.tsx
+++ src/components/layout/footer/footer-three.tsx
@@ -25,7 +25,6 @@ export function Footer({ siteSettings }: { siteSettings: any }) {
                 <div className="border-t border-zinc-100 pb-6 pt-6 dark:border-zinc-700/40">
                     <ContainerInner>
                         <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-                            
                             <div className="flex items-center gap-6 whitespace-nowrap text-sm font-medium text-zinc-800 dark:text-zinc-200">
                                 {siteSettings?.linkList == null ? (
                                     <></>
@@ -45,7 +44,26 @@ export function Footer({ siteSettings }: { siteSettings: any }) {
                                 )}
                             </div>
 
-                            <Copyright />
+                            <div className="text-center text-xs lg:text-sm leading-6 theme-text-description">
+                                &copy; {new Date().getFullYear()}{" "}
+                                <span className="font-semibold">
+                                    Spectrum Psychiatry.
+                                </span>{" "}
+                                All rights reserved
+                                <br />
+                                <span className="text-[12px] opacity-70">
+                                    Powered by{" "}
+                                    <a
+                                        href="https://www.youtube.com/watch?v=cAe1lVDbLf0"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="font-semibold underline transition hover:text-blue-500 hover:opacity-100"
+                                    >
+                                        Keremiyo 🎵
+                                    </a>{" "}
+                                    <span>🔥</span>
+                                </span>
+                            </div>
                         </div>
                     </ContainerInner>
                 </div>
