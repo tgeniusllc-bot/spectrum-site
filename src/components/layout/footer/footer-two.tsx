53aac1ccf2005d9e9ac47eaae874ee64c1de8d46
Kerem <tgeniusllc@gmail.com>
2026-05-16 18:44:03 +0300

    test footer-two live

--- src/components/layout/footer/footer-two.tsx
+++ src/components/layout/footer/footer-two.tsx
@@ -1,6 +1,7 @@
 import Widgets from './widgets'
 import Copyright from './copyright'
 import { footerContemporary } from './data'
+
 const { widgets, payment } = footerContemporary
 
 const FooterTwo: React.FC = () => {
@@ -8,7 +9,20 @@ const FooterTwo: React.FC = () => {
     <footer className='border-b-4 border-heading mt-9 md:mt-11 lg:mt-16 3xl:mt-20 pt-2.5 lg:pt-0 2xl:pt-2'>
       <Widgets widgets={widgets} variant='contemporary' />
       <Copyright payment={payment} variant='contemporary' />
+
+      {/* TEST */}
+      <div
+        style={{
+          textAlign: "center",
+          color: "red",
+          fontWeight: "bold",
+          paddingBottom: "10px",
+        }}
+      >
+        FOOTER TEST KEREMIYO
+      </div>
     </footer>
   )
 }
+
 export default FooterTwo
